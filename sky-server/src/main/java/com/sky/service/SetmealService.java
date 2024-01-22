a3420ead177749e4aebdd2a43df274fc2b412ec0
zj <809267697@qq.com>
2024-01-22 15:19:26 +0800

    update 添加套餐和套餐中的菜品

---
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -0,0 +1,12 @@
+package com.sky.service;
+
+import com.sky.dto.SetmealDTO;
+
+public interface SetmealService {
+
+    /**
+     * 保存套餐
+     * @param setmealDTO
+     */
+    void save(SetmealDTO setmealDTO);
+}
